5dee215221079916982ba1a41420c30dc81cf8ac
Artur Borges <rafaelnogueira99843@gmail.com>
2026-08-21 20:19:22 -0300

    Adiciona controller base à nova namespace Presentation
    Foram adicionadas as diretivas using para Microsoft.AspNetCore.Http e Microsoft.AspNetCore.Mvc. Criada a namespace Projetos___4._1___Presentation.Controllers com uma classe Controller herdando de ControllerBase, marcada com [Route("api/[controller]")] e [ApiController], sem métodos ou propriedades implementados.

---
+++ Projetos - 4/1 - Presentation/Controllers/Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projetos___4._1___Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+    }
+}
