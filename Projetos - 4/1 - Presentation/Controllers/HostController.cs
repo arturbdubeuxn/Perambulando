9a206bf2b039765b8a1dc3de3109e00f9cd1f200
Artur Borges <rafaelnogueira99843@gmail.com>
2026-08-26 12:53:58 -0300

    Remove endpoints de previsão do tempo e adiciona Host
    Removidos Controller.cs, WeatherForecastController.cs e WeatherForecast.cs, eliminando endpoints e modelos de previsão do tempo. Adicionados HostController.cs com novo endpoint "api/Host" e classe Host.cs vazia. Atualizado Projetos - 4.http para refletir as mudanças nos endpoints.

--- Projetos - 4/1 - Presentation/Controllers/HostController.cs
+++ Projetos - 4/1 - Presentation/Controllers/HostController.cs
@@ -5,7 +5,12 @@ namespace Projetos___4._1___Presentation.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class Controller : ControllerBase
+    public class HostController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from the Host Controller!");
+        }
     }
 }
---
+++ Projetos - 4/3 - Domain/Model/Host.cs
@@ -0,0 +1,6 @@
+﻿namespace Projetos___4._3___Domain.Model
+{
+    public class Host
+    {
+    }
+}
